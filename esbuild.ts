2c1ddf17c16bc32f4741a007d9d55780c05089c1
Eugene Zinovyev <eugene.zinovyev@gmail.com>
2023-08-13 16:08:12 +0400

    Initialized the repository

---
+++ esbuild.ts
@@ -0,0 +1,19 @@
+﻿import { build as esbuild, type BuildOptions, type BuildResult } from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
+
+function build(options: BuildOptions): Promise<BuildResult> {
+    return esbuild({
+        bundle: true,
+        minify: true,
+        platform: 'node',
+        target: 'node16',
+        plugins: [nodeExternalsPlugin()],
+        ...options,
+    });
+}
+
+await Promise.all([
+    build({ entryPoints: ['src/cli.ts'], format: 'esm', outdir: 'dist', banner: { js: '#!/usr/bin/env node' } }),
+    build({ entryPoints: ['src/api.ts'], format: 'esm', outfile: 'dist/api.mjs' }),
+    build({ entryPoints: ['src/api.ts'], format: 'cjs', outfile: 'dist/api.cjs' }),
+]);
